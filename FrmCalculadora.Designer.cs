2bb10f2f0b6be3ade04dd703443a9f2665151838
VitoriaAlvesR <vitoriaalves.ajax@gmail.com>
2024-09-17 13:44:08 -0300

    Add: Atualizando o icon da calculadora

--- FrmCalculadora.Designer.cs
+++ FrmCalculadora.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCalculadora));
+            this.SuspendLayout();
+            // 
+            // FrmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(413, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmCalculadora";
             this.Text = "FrmCalculadora";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
