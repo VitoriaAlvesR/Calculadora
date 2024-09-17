9ef3ebf13e5a725649d08f3a9cbdd79ab30bd5d0
VitoriaAlvesR <vitoriaalves.ajax@gmail.com>
2024-09-17 11:23:51 -0300

    Modify: Alteração do Timer Add: Comandos de tela inseridos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pgrsCarregando = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,13 +46,20 @@
             this.pictureBox1.Size = new System.Drawing.Size(891, 441);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // progressBar1
+            // pgrsCarregando
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 390);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(855, 35);
-            this.progressBar1.TabIndex = 3;
+            this.pgrsCarregando.BackColor = System.Drawing.Color.LightGray;
+            this.pgrsCarregando.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pgrsCarregando.Enabled = false;
+            this.pgrsCarregando.ForeColor = System.Drawing.Color.Lime;
+            this.pgrsCarregando.Location = new System.Drawing.Point(0, 402);
+            this.pgrsCarregando.Maximum = 10;
+            this.pgrsCarregando.Name = "pgrsCarregando";
+            this.pgrsCarregando.Size = new System.Drawing.Size(880, 35);
+            this.pgrsCarregando.TabIndex = 3;
+            this.pgrsCarregando.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // label1
             // 
@@ -60,10 +69,16 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(12, 343);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(233, 44);
+            this.label1.Size = new System.Drawing.Size(227, 43);
             this.label1.TabIndex = 4;
             this.label1.Text = "Carregando...";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -72,7 +87,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(880, 437);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pgrsCarregando);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -81,6 +96,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -89,8 +105,9 @@
 
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pgrsCarregando;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
